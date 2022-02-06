be30e412bba459969dca1af37e3f963565a90249
keremersann <91527036+keremersann@users.noreply.github.com>
2022-02-07 01:22:15 +0300

    Database connected. Category model class is added. Category controller added.

---
+++ BulkyBookWeb/Data/BulkyContext.cs
@@ -0,0 +1,19 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBookWeb.Data
+{
+    public class BulkyContext : DbContext
+    {
+        public BulkyContext(DbContextOptions<BulkyContext> options) :base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
